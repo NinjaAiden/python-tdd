6c9f724355cc0f2de3f54141335a82e756c847b2
Aiden Harvey <aidenharvey102@gmail.com>
2019-01-09 11:42:44 +0000

    added test for exceptions and added limiter to number of coins available

--- byotest.py
+++ byotest.py
@@ -13,32 +13,32 @@ def test_not_in(collection, item):
 def test_between(lower, upper, actual):
     assert lower <= actual <= upper, "{1} is not between {0} and {2}".format(lower, actual, upper)
     
-# test fails    
-#test_are_equal(8, 9)
+def test_exception_was_raised(func, args, message):
+    """
+    Test that an error gets thrown inside of a given function. Raises
+    AssertionError if the error message was different from the expected
+    message
+    `func` is a reference to the function that is to be called
+    `args` is a tuple containing the arguments that are to be provided to
+        `func`
+    `message` is the string that is expected to be output by the error once
+        it's thrown
+    """
+    try:
+        # Call the function and unpack the `args` tuple by using `*`. This
+        # will unpack each of the items from the `args` tuple to pass 
+        # them into the function as arguments
+        func(*args)
 
-# test passes
-test_are_equal(8, 8)
-
-# test fails
-#test_not_equal(5, 5)
-
-# test passes
-test_not_equal(5, 7)
-
-# test fails
-#test_is_in([1, 2, 3, 4, 5], 6)
-
-# test passes
-test_is_in([1, 2, 3, 4, 5], 3)
-
-# test fails
-#test_not_in([1, 2, 3, 4, 5, 6], 5)
-
-# test passes
-test_not_in([1, 2, 3, 4, 5, 6], 9)
-
-# test fails
-#test_between(0, 8, 15)
-
-# test pases
-test_between(0, 15, 8)
+        # Execution will cease at this point if the error was successfully
+        # thrown, and will move onto the `except` block. If the
+        # function was successfully executed without throwing an error, we'll
+        # raise an AssertionError here to inform the developer that the
+        # function didn't throw an error as expected
+        assert False, "Exception was not raised"
+    except Exception as e:
+        # The message that was thrown will be stored in the exception
+        # instance as the first item in the list of `args`. This will allow us
+        # to check to see if the message that was thrown is the same as the
+        # message that the developer was expecting 
+        assert e.args[0] == message, "The message that was provided did not match the message thrown"
